31abe00ef97e952aecb29ce0c37d6f8fd24d25ce
Trkiller02 <trollkiller0207@gmail.com>
2024-04-18 16:16:18 -0400

    fix base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Trkiller02.github.io",
-  base: "/omarmarquezcv",
+  base: "/minimalist-portfolio",
   output: "static",
   integrations: [tailwind()],
 });
