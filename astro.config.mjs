a3818ea8d62f812fb97d1a6325077cd966a8254a
Trkiller02 <trollkiller0207@gmail.com>
2025-05-24 15:33:00 -0400

    style!:UpgradeTailwindCSS4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://trkiller02.github.io/",
-	base: "minimalist-portfolio",
-	output: "static",
-	integrations: [tailwind()],
+	/* site: "https://trkiller02.github.io/",
+	base: "minimalist-portfolio", */
 	i18n: {
 		defaultLocale: "es",
 		locales: ["es", "en"],
 		routing: {
 			prefixDefaultLocale: false,
 		},
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 });
