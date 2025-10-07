d5e2a5d1db8acd3018c48b2379d4278dc3e6cc75
Trkiller02 <trollkiller0207@gmail.com>
2025-10-07 00:03:53 -0400

    feat(pages): add courses section

---
+++ src/db/coursesList.ts
@@ -0,0 +1,15 @@
+import react from "./diploma-react.webp";
+import javascript from "./diploma-javascript.webp";
+import typescript from "./diploma-typescript.webp";
+import javascriptCleanCode from "./diploma-javascript-clean-code.webp";
+import typescriptTips from "./diploma-typescript-tipos-avanzados.webp";
+import frontendDeveloper from "./diploma-frontend-developer.webp";
+
+export const COURSES_IMAGES = {
+	REACT: react,
+	JAVASCRIPT: javascript,
+	TYPESCRIPT: typescript,
+	CLEAN_CODE: javascriptCleanCode,
+	TYPESCRIPT_TIPS: typescriptTips,
+	FRONTEND_DEVELOPER: frontendDeveloper,
+} as const;
